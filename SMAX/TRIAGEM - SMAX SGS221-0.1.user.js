e60427ef5e661fc8f3e0b4039793be57fcc02f08
Daniel <danielmaccruz@yahoo.com>
2026-04-15 09:32:15 -0300

    Auto update 2026-15-04 9-32

--- SMAX/TRIAGEM - SMAX SGS221-0.1.user.js
+++ SMAX/TRIAGEM - SMAX SGS221-0.1.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TRIAGEM - SMAX SGS221
 // @namespace    https://github.com/DanielMacCruz/SGS221-Triagem
-// @version      1.01
+// @version      1.02
 // @description  Interface enhancements for triagem workflow
 // @author       YOU
 // @match        https://suporte.tjsp.jus.br/saw/Requests*
